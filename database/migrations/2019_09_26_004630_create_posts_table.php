058bd273b5b4702a13f9a0cba65aae3de1ee8f37
Salar Pourfallah <39343928+salarsp@users.noreply.github.com>
2019-09-26 08:20:46 +0330

    Database tables
    Creating tables of database

---
+++ database/migrations/2019_09_26_004630_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 300);
+            $table->string('content', 5000);
+            $table->string('image', 300);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
