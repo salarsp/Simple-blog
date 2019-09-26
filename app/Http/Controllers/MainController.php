0ce6a6f4c3994f37f8070b89b880a142fde71f47
Salar Pourfallah <39343928+salarsp@users.noreply.github.com>
2019-09-27 00:11:09 +0330

    Message and Replies
    Sending messages and replies by users

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Comment;
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
 {
@@ -21,5 +23,37 @@ class MainController extends Controller
 //            return to main page if post does not exist
             return redirect('/');
         }
+    }
+
+    public function add(Request $request)
+    {
+        try {
+//            getting post id and comment and replying comment id
+            $id = $request->get('id');
+            $content = $request->get('content');
+            $reply = $request->get('reply');
+
+            $comment = new Comment();
+            $comment->user_id = Auth::user()->id;
+            $comment->post_id = $id;
+            $comment->content = $content;
+            $comment->reply_to_comment_id = $reply;
+            $comment->save();
+
+            session()->put('message', 'Your message sent successfully.');
+            session()->put('type', 'success');
+        } catch (\Exception $exception) {
+//            return to main page if post does not exist
+            session()->put('message', 'Failed to send comment.');
+            session()->put('type', 'warning');
+        }
+        return back();
+    }
+
+//     Admin Panel function
+    public function admin(){
+        $posts = Post::orderBy('id','desc')->paginate(10);
+        $comments = Comment::orderBy('id','desc')->paginate(10);
+        return view('admin', compact('posts', 'comments'));
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -18,26 +18,44 @@
                     <div class="card-body">
                         <h4 id="add" class="text-center">Comments</h4>
                         <hr>
-                        <p class="font-weight-normal">Write your comment:</p>
-                        <form action="{{route('new_comment')}}" method="post">
-                            @csrf
-                            <div id="replyArea" class="mb-1" style="display: none">
-                                <b>Replying to:</b>
-                                <input style="display: none"
-                                       onclick="RemoveReply()" type="button"
-                                       id="reply" class="btn btn-light"
-                                       data-toggle="tooltip" data-placement="top" title="Click to remove reply">
+                        @if(session('message'))
+                            <div class="alert alert-{{session('type')}} alert-dismissible fade show" role="alert">
+                                {{session('message')}}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
                             </div>
-                            <textarea name="content" id="content" cols="1" rows="5" class="form-control"></textarea>
-                            <button class="btn btn-block btn-primary mt-2" type="submit">Send</button>
-                        </form>
+                            @php(session()->forget('message'))
+                            @php(session()->forget('type'))
+                        @endif
+                        @auth
+                            <p class="font-weight-normal">Write your comment:</p>
+                            <form action="{{route('new_comment')}}" method="post">
+                                @csrf
+                                <input type="hidden" hidden name="id" value="{{$post->id}}">
+                                <input id="rep2" type="hidden" hidden name="reply">
+                                <div id="replyArea" class="mb-1" style="display: none">
+                                    <b>Replying to:</b>
+                                    <input style="display: none"
+                                           onclick="RemoveReply()" type="button"
+                                           id="reply" class="btn btn-light"
+                                           data-toggle="tooltip" data-placement="top" title="Click to remove reply">
+                                </div>
+                                <textarea required name="content" id="content" cols="1" rows="5"
+                                          class="form-control"></textarea>
+                                <button class="btn btn-block btn-primary mt-2" type="submit">Send</button>
+                            </form>
+                        @else
+                            <p class="font-weight-bold text-secondary"><a href="{{route('login')}}">Login</a> to your account or <a href="{{route('register')}}">Register</a> if you don't have an account to send comment.</p>
+                        @endauth
                         <div class="my-3">
                             @foreach($post->comments as $comment)
                                 @if($comment->reply_to_comment_id == '')
                                     <div class="comment p-3 my-2">
                                         <div class="row">
                                             <div class="col-sm-4 col-md-2">
-                                                <img class="img img-thumbnail rounded-circle" src="{{asset('images/user.png')}}"
+                                                <img class="img img-thumbnail rounded-circle"
+                                                     src="{{asset('images/user.png')}}"
                                                      alt="{{$comment->user->name}}">
                                             </div>
                                             <div class="col-sm-8 com-md-10">
@@ -77,12 +95,14 @@
     <script>
         function ReplyTo(destination) {
             document.getElementById('reply').value = destination;
+            document.getElementById('rep2').value = destination;
             document.getElementById('reply').style.display = '';
             document.getElementById('replyArea').style.display = '';
         }
 
         function RemoveReply() {
             document.getElementById('reply').value = '';
+            document.getElementById('rep2').value = '';
             document.getElementById('reply').style.display = 'none';
             document.getElementById('replyArea').style.display = 'none';
         }
